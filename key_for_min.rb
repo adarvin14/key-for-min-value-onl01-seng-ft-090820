a486288b8438571bccf35b10952312b706bcd8ab
Alex Darvin <amdarvin14@gmail.com>
2020-09-18 16:59:15 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,5 +4,6 @@
 def key_for_min_value(name_hash)
   return nil if name_hash.size == 0
     min.key, min.value = name_hash.first
-    name_hash.each do |
+    name_hash.each do |key, value|
+      
 end
